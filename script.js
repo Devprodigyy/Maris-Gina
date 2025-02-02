3b69062cbeb7083bd0cb5a70fc2b96d1b734b025
Kunal watulkar <145703445+Devprodigyy@users.noreply.github.com>
2025-02-02 18:10:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function loco() {
   const locoScroll = new LocomotiveScroll({
     el: document.querySelector("main"),
     smooth: true,
-    lerp: 0.005,
+    lerp: 0.1,
   });
   // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
   locoScroll.on("scroll", ScrollTrigger.update);
